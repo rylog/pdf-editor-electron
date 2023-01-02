dc0039342ae8ba341ed0ad40678b373794c8e387
Ryan <ryan.lo@polymtl.ca>
2023-01-02 14:07:15 -0500

    create pdf loader service

---
+++ src/services/PdfLoader.ts
@@ -0,0 +1,52 @@
+import { pdfjs,} from "react-pdf";
+import {
+  GetDocumentParameters,
+  PDFDocumentProxy,
+  PDFPageProxy, 
+} from "pdfjs-dist/types/src/display/api";
+
+const loadDocumentPages = (arrayBuffer: GetDocumentParameters) => {
+  return new Promise<PDFPageProxy[]>((resolve, reject) => {
+    try {
+      pdfjs
+        .getDocument(arrayBuffer)
+        .promise.then((pdfDocument: PDFDocumentProxy) => {
+          //get pages from the document
+          const getPageTasks:Promise<PDFPageProxy>[] = [];
+          for (let i = 0; i < pdfDocument.numPages; i++) {
+            getPageTasks.push(pdfDocument.getPage(i + 1));
+          }
+          Promise.all<PDFPageProxy>(getPageTasks).then((pages) => resolve(pages));
+        });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const readAsArrayBuffer = (file: File) => {
+  return new Promise<Uint8Array>((resolve, reject) => {
+    const filereader = new FileReader();
+    filereader.onload = () => {
+      resolve(new Uint8Array(filereader.result as ArrayBuffer));
+    };
+    filereader.onerror = () => reject(filereader);
+    filereader.readAsArrayBuffer(file);
+  });
+};
+
+const readFilesAsArrayBuffer = (files: FileList) => {
+  const readers: Promise<Uint8Array>[] = [];
+  //Read all inputs
+  Array.from(files).forEach((file) => {
+    readers.push(readAsArrayBuffer(file));
+  });
+
+  return Promise.all(readers);
+};
+const PdfLoader = {
+  loadDocumentPages,
+  readFilesAsArrayBuffer
+}
+
+export default PdfLoader;
