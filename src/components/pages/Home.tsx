1134c236a41d026f10170fdb799a2f9fe154acc3
Ryan <ryan.lo@polymtl.ca>
2023-02-19 18:39:54 -0500

    Refactor (use custom hook and fileInputButton)

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,22 +1,16 @@
-import classes from "./Home.module.css"
-import Folder from '@mui/icons-material/Folder'
-import { ChangeEvent, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Button, Paper } from '@mui/material';
+import classes from "./Home.module.css";
+import Folder from "@mui/icons-material/Folder";
+import { ChangeEvent, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Paper } from "@mui/material";
+import FileInputButton from "../ui/FileInputButton";
 
 const Home = () => {
-
   const navigate = useNavigate();
-  const hiddenFileInput = useRef<HTMLInputElement>(null);
-
-  const browseFiles = () => {
-    if (hiddenFileInput.current)
-      hiddenFileInput.current.click();
-  }
-
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
-    navigate('/editor', { state: { files: files } });
+    console.log(files)
+    navigate("/editor", { state: { files: files } });
   };
 
   return (
@@ -25,20 +19,17 @@ const Home = () => {
         <Folder color="primary" sx={{ fontSize: 108 }}></Folder>
         <h1>Drag your PDF files here</h1>
         <p className={classes.lined}>OR</p>
-        <Button sx={{ mt: 2}} className={classes.browseBtn} color="primary" variant="contained"  onClick={browseFiles} >
-        Browse files
-      </Button>
-      <input
-        className={classes.fileInput}
-        type="file"
-        ref={hiddenFileInput}
-        multiple
-        onChange={(event) => {
-          handleInputChange(event);
-        }}
-      />
-    </Paper>
-    </div >
+        <FileInputButton
+          sx={{ mt: 2 }}
+          className={classes.browseBtn}
+          color="primary"
+          variant="contained"
+          onFileChange={(event) => handleInputChange(event)}
+        >
+          Browse files
+        </FileInputButton>
+      </Paper>
+    </div>
   );
 };
 
